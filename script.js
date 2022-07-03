ccd9ed33ac4678051d0d6c855ff45bbf8ada5bc9
Jason Greenwald <34841215+jaycgreenwald@users.noreply.github.com>
2022-07-02 17:03:51 -0700

    7/2/2022 - Combine CSS, new error msg
    I combined the two css files (one for mobile and one for desktop) into a single file.
    I added a new error message for when the email field is left blank.

--- script.js
+++ script.js
@@ -11,13 +11,17 @@ function validateEmail() {
     const inputEmail = document.getElementById("email").value;
     const isValidEmail = emailRegex.test(inputEmail);
     
-    // document.getElementById("email").placeholder = "example@email/com";
-
     if (isValidEmail == false) {
-        document.querySelector('.error').style.display = 'block';
         const inputField = document.getElementById("email");
         inputField.value = "example@email/com";
         inputField.style.border = "1px solid var(--clr-red-400)";
+        if (inputEmail == '') {
+            document.querySelector('.error-format').style.display = 'none';
+            document.querySelector('.error-empty').style.display = 'block';
+        } else {
+            document.querySelector('.error-empty').style.display = 'none';
+            document.querySelector('.error-format').style.display = 'block';
+        }
     }
     
 }
